2277cd056d8231896bd1b19cd80d6b6723bbc4ab
zhouzf <zzfcasic@163.com>
2020-04-06 16:23:49 +0800

    remove --clone_on_cpu

--- research/run.sh
+++ research/run.sh
@@ -35,7 +35,7 @@ do
     sed -i "s/^  num_steps: $last$/  num_steps: $current/g" $pipeline_config_path  # 通过num_steps控制一次训练最多100step
 
     echo "############" $i "training #################"
-    python ./object_detection/train.py --train_dir=$train_dir --pipeline_config_path=$pipeline_config_path --clone_on_cpu=False
+    python ./object_detection/train.py --train_dir=$train_dir --pipeline_config_path=$pipeline_config_path
 
     echo "############" $i "evaluating, this takes a long while #################"
     python ./object_detection/eval.py --checkpoint_dir=$checkpoint_dir --eval_dir=$eval_dir --pipeline_config_path=$pipeline_config_path
